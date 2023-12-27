c4b2673ccb5fdf67eb6b6ce9d53d878de1f09976
Oyera1 <7.chiacherini@gmail.com>
2023-12-27 02:39:10 -0300

    adicionando responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { fetchMovieData, randomId } from "@/api/api";
 import Resume from "@/components/resumes";
-import { PiSunDimBold } from "react-icons/pi";
 import { useMovie } from "../../context/MovieContext";
 
 export default function Home() {
   const { movieData, getMovieData } = useMovie();
 
   return (
-    <main className="flex flex-col flex-wrap h-screen px-28 justify-center items-center bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-slate-700 via-slate-800 to-slate-900">
+    <main
+      className="flex flex-col flex-wrap min-h-screen max-h-max px-6 py-3 sm:px-28 justify-center items-center
+      bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))]
+     from-slate-700 via-slate-800 to-slate-900"
+    >
       <div
         className="flex flex-col items-center py-5 px-10 bg-zinc-500 rounded-md
         backdrop-filter backdrop-blur-sm bg-opacity-10"
@@ -17,7 +19,7 @@ export default function Home() {
         {movieData ? (
           <Resume />
         ) : (
-          <span className="mb-16 font-semibold text-xl">
+          <span className="mb-16 font-semibold text-xl text-center">
             Não sabe o que assistir?
           </span>
         )}
@@ -28,10 +30,15 @@ export default function Home() {
         >
           Encontrar filme!
         </button>
-        <span className="text-center break-before-auto">
-          Clique em "Encontrar filme" que te traremos informações de algum filme
-          para você assistir hoje!
-        </span>
+
+        {movieData ? (
+          ""
+        ) : (
+          <span className="text-center break-before-auto">
+            Clique em "Encontrar filme" que te traremos informações de algum
+            filme para você assistir hoje!
+          </span>
+        )}
       </div>
     </main>
   );
--- src/components/resumes.tsx
+++ src/components/resumes.tsx
@@ -5,19 +5,19 @@ import { MovieContext } from "../../context/MovieContext";
 export default function Resume() {
   const { movieData } = useContext(MovieContext);
   return (
-    <main className="flex flex-wrap flex-col items-center gap-10 m-10 w-[1000px] h-[400px]">
-      <span className="font-bold text-2xl">
+    <main className="flex flex-wrap flex-col items-center gap-10 sm:m-10 sm:w-[1000px] sm:h-[400px]">
+      <span className="font-bold text-2xl text-center">
         {movieData ? movieData.title : "carregando..."}
       </span>
-      <div className="flex flex-col sm:flex-row gap-16 items-start px-10">
+      <div className="flex flex-wrap flex-col sm:flex-row gap-6 sm:gap-16 items-center sm:items-start sm:px-10">
         <Image
           src={movieData ? movieData.poster : "carregando..."}
           alt={movieData ? movieData.title : "carregando..."}
           height={300}
           width={200}
-          className="sm:max-w-max sm:max-h-max"
+          className="max-w-max max-h-max rounded-xl"
         />
-        <span className="">
+        <span className="text-center mb-4 sm:mb-0 sm:text-start ">
           {movieData
             ? movieData.overview == ""
               ? "Este filme não possui um resumo."
